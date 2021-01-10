12bc105ed326bf0ea37d09e3bfbc57c97c338459
Lukas <biruta.lukas@gmail.com>
2021-01-10 09:18:15 +0200

    Announcement Bar

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,18 @@
-// Put your application javascript here
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // Announcement Bar Manipulation
+    let announcementBar = document.querySelector('.announcement');
+    let closeAnnouncementBar = document.querySelector('.announcement__close');
+
+    setTimeout(function(){
+        announcementBar.classList.remove('hidden')
+    }, 3000);
+
+    closeAnnouncementBar.addEventListener("click", function(){
+        announcementBar.classList.add('hidden');
+    })
+});
+
+
