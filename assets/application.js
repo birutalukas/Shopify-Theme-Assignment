f76b73983868fea609a7294a0cae6c82e9a06ab8
Lukas <biruta.lukas@gmail.com>
2021-01-11 10:26:52 +0200

    Assignment Completed

--- assets/application.js
+++ assets/application.js
@@ -5,14 +5,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Announcement Bar Manipulation
     let announcementBar = document.querySelector('.announcement');
     let closeAnnouncementBar = document.querySelector('.announcement__close');
+    
+    if (announcementBar != null) {
+        setTimeout(function(){
+            announcementBar.classList.remove('hidden')
+        }, 3000);
+    }
 
-    setTimeout(function(){
-        announcementBar.classList.remove('hidden')
-    }, 3000);
+    if (closeAnnouncementBar != null) {
+        closeAnnouncementBar.addEventListener("click", function(){
+            announcementBar.classList.add('hidden');
+        });
+    }
 
-    closeAnnouncementBar.addEventListener("click", function(){
-        announcementBar.classList.add('hidden');
-    })
 });
 
 
